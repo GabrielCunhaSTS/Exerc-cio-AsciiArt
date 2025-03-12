6ff79b8ed15952eaa661748ee2f8d79f14b11a00
Gabriel <ggabrielcunha06@gmail.com>
2025-03-12 17:35:29 -0300

    asciiart basquete

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("             ________");
+Console.WriteLine("     o      |   __   |");
+Console.WriteLine("      \\_ O  |  |__|  |");
+Console.WriteLine("   ____/ \\  |___WW___|");
+Console.WriteLine("   __/   /      ||");
+Console.WriteLine("                ||");
+Console.WriteLine("                ||");
+Console.WriteLine("________________||________________");
